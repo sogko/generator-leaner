f94f4bd944512f2dbc2ee4e4a38dc89076fc0eb8
Hafiz Ismail <sgk.sprm@gmail.com>
2014-08-08 00:01:38 +0800

    Started code refactoring / clean up

--- app/index.js
+++ app/index.js
@@ -1,24 +1,48 @@
 'use strict';
 var util = require('util');
-var path = require('path');
+var async = require('async');
 var yeoman = require('yeoman-generator');
-var yosay = require('yosay');
-var chalk = require('chalk');
+
+var Messages = require('../lib/generator-messages');
+var Utils = require('../lib/generator-utils');
+var Paths = require('../lib/generator-paths');
 
 var LeanerGenerator = module.exports = function LeanerGenerator(args, options) {
   yeoman.generators.Base.apply(this, arguments);
 
-  // init app name
-  this.argument('appname', { type: String, required: false });
-  this.appname = this.appname || path.basename(process.cwd());
-  this.appname = this._.camelize(this._.slugify(this._.humanize(this.appname)));
+  // init LEANER project name
+  this.projectName = this.projectName || path.basename(process.cwd());
+  this.projectName = this._.camelize(this._.slugify(this._.humanize(this.projectName)));
+  this.appname = this.projectName; // alias it to 'appname'
 
+  // load generator package.json
   this.pkg = require('../package.json');
 
+  // define arguments
+  this.argument('projectName', {
+    desc: 'LEANER project name',
+    defaults:  path.basename(process.cwd()),
+    type: String,
+    required: false
+  });
+
+  // define options
+  this.option('skip-install', {
+    desc: 'Skip installing NPM and Bower dependencies',
+    type: Boolean,
+    default: false
+  });
+
+  this.option('skip-welcome-message', {
+    desc: 'Skip welcome message',
+    type: Boolean,
+    default: false
+  });
+
+  // install dependencies after completed generating project scaffold
   this.on('end', function () {
-    var endMessage =function endMessage() {
-      this.log('\n\n' + chalk.green('success') + chalk.white(' Your LEANER app ' + chalk.bold(this.appname)+ ' has been created.'));
-      this.log(chalk.green('Run ' + chalk.white.bold('npm start')+' to serve the app\n\n'));
+    var endMessage = function endMessage() {
+      this.log(Messages(this).app.completed());
     }.bind(this);
     if (!this.options['skip-install']) {
       this.installDependencies({
@@ -34,63 +58,29 @@ util.inherits(LeanerGenerator, yeoman.generators.Base);
 
 LeanerGenerator.prototype.welcome = function welcome() {
   if (!this.options['skip-welcome-message']) {
-    this.log(yosay('Welcome! \nLet\'s get leaner, my friend!'));
-    this.log(
-      chalk.magenta([
-          'LEANER is a Lightweight & Leaner out-of-the-box ExpressJS-AngularJS-NodeJS-RequireJS Stack',
-          '\n'
-        ].join('')
-      )
-    );
+    this.log(Messages(this).app.welcome());
   }
 };
 
-LeanerGenerator.prototype.generateAppRootFiles = function appRootFiles() {
-  this._processDirectory('_root', '');
+LeanerGenerator.prototype.generateProjectScaffold = function generateProjectScaffold() {
+  async.series([
+    function generateProjectRoot(next) {
+      this.log(Messages(this).app.generateProjectRoot());
+      Utils(this).copyTemplateDirectory('_root', Paths.dest.root);
+      next();
+    }.bind(this),
+    function generateServerComponents(next) {
+      this.log(Messages(this).app.generateServerComponents());
+      Utils(this).copyTemplateDirectory('server', Paths.dest.server.root);
+      next();
+    }.bind(this),
+    function generateClientComponents(next) {
+      this.log(Messages(this).app.generateClientComponents());
+      Utils(this).copyTemplateDirectory('client', Paths.dest.client.root);
+      next();
+    }.bind(this)
+  ], function () {
+
+  });
 };
 
-LeanerGenerator.prototype.generateServerScaffold = function generateScaffold() {
-  this._processDirectory('server', 'server');
-};
-
-LeanerGenerator.prototype.generateClientScaffold = function generateScaffold() {
-  this._processDirectory('client', 'client');
-//  this.mkdir('client/app');
-};
-
-LeanerGenerator.prototype._askFor = function askFor() {
-
-  var done = this.async();
-
-  var prompts = [{
-    type: 'confirm',
-    name: 'someOption',
-    message: 'Would you like to enable this option?',
-    default: true
-  }];
-
-  this.prompt(prompts, function (props) {
-    this.someOption = props.someOption;
-
-    done();
-  }.bind(this));
-
-};
-
-LeanerGenerator.prototype._processDirectory = function(source, destination) {
-  var root = this.isPathAbsolute(source) ? source : path.join(this.sourceRoot(), source);
-  var files = this.expandFiles('**', { dot: true, cwd: root });
-
-  for (var i = 0; i < files.length; i++) {
-    var f = files[i];
-    var src = path.join(root, f);
-    if(path.basename(f).indexOf('_') == 0){
-      var dest = path.join(destination, path.dirname(f), path.basename(f).replace(/^_/, ''));
-      this.template(src, dest);
-    }
-    else{
-      var dest = path.join(destination, f);
-      this.copy(src, dest);
-    }
-  }
-};

28912c658ee3cb111f79d70d1d56fc5f7c76d79d
Hafiz Ismail <sgk.sprm@gmail.com>
2014-08-08 15:57:49 +0800

    Rewrote Messages as a mixin

--- app/index.js
+++ app/index.js
@@ -4,28 +4,24 @@ var path = require('path');
 var async = require('async');
 var yeoman = require('yeoman-generator');
 
-var Messages = require('../lib/generator-messages');
+
+var MessagesMixin = require('../lib/mixins/messages');
 var Utils = require('../lib/generator-utils');
 var Paths = require('../lib/generator-paths');
 
 var LeanerGenerator = module.exports = function LeanerGenerator(args, options) {
   yeoman.generators.Base.apply(this, arguments);
 
-  // init LEANER project name
-  this.projectName = this.projectName || path.basename(process.cwd());
-  this.projectName = this._.camelize(this._.slugify(this._.humanize(this.projectName)));
-  this.appname = this.projectName; // alias it to 'appname'
-
   // load generator package.json
   this.pkg = require('../package.json');
 
-  // define arguments
   this.argument('projectName', {
     desc: 'LEANER project name',
     defaults:  path.basename(process.cwd()),
     type: String,
     required: false
   });
+  this.projectName = Paths(this).currentProjectName();
 
   // define options
   this.option('skip-install', {
@@ -46,43 +42,44 @@ var LeanerGenerator = module.exports = function LeanerGenerator(args, options) {
 };
 
 util.inherits(LeanerGenerator, yeoman.generators.Base);
+util._extend(LeanerGenerator.prototype, MessagesMixin);
 
 LeanerGenerator.prototype.welcome = function welcome() {
   if (!this.options['skip-welcome-message']) {
-    this.log(Messages(this).app.welcome());
+    this.messages('app.welcome');
   }
 };
 
 LeanerGenerator.prototype.generateProjectScaffold = function generateProjectScaffold() {
   async.series([
     function generateProjectRoot(next) {
-      this.log(Messages(this).app.generateProjectRoot());
+      this.messages('app.generateProjectRoot');
       Utils(this).copyTemplateDirectory('_root', Paths.dest.root);
       next();
     }.bind(this),
     function generateServerComponents(next) {
-      this.log(Messages(this).app.generateServerComponents());
+      this.messages('app.generateServerComponents');
       Utils(this).copyTemplateDirectory('server', Paths.dest.server.root);
       next();
     }.bind(this),
     function generateClientComponents(next) {
-      this.log(Messages(this).app.generateClientComponents());
+      this.messages('app.generateClientComponents');
       Utils(this).copyTemplateDirectory('client', Paths.dest.client.root);
       next();
     }.bind(this),
     function installDependencies(next) {
       if (!this.options['skip-install']) {
-        this.log(Messages(this).app.installDependencies());
+        this.messages('app.installDependencies');
         this.installDependencies({
           callback: function() { next(); }
         });
       } else {
-        this.log(Messages(this).app.skippedInstallDependencies());
+        this.messages('app.skippedInstallDependencies');
         next();
       }
     }.bind(this),
     function completed(next) {
-      this.log(Messages(this).app.completed());
+      this.messages('app.completed');
       next();
     }.bind(this)
   ], function () {

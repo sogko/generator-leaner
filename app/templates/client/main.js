659bdb430df4ab64d6ab3c3728a1e6ab11625766
Hafiz Ismail <sgk.sprm@gmail.com>
2014-08-10 09:08:30 +0800

    Commit :app template for /client

---
+++ app/templates/client/main.js
@@ -0,0 +1,36 @@
+/**
+ * Main entry-point for client side applications
+ *
+ * By default, this file is loaded by /server/views/layouts/main.hbs
+ */
+'use strict';
+
+require.config({
+  baseUrl: '/',
+
+  paths: {
+    'angular': 'assets/js/vendor/angular/angular',
+    'ui.router': 'assets/js/vendor/angular-ui-router/release/angular-ui-router',
+    'domReady': 'assets/js/vendor/requirejs-domready/domReady',
+    'jquery': 'assets/js/vendor/jquery/dist/jquery',
+    'bootstrap': 'assets/js/vendor/jquery/dist/jquery'
+  },
+
+  packages: [
+    // define additional ng-app packages here
+  ],
+
+  shim: {
+    'angular': {
+      'deps': ['jquery'],
+      'exports': 'angular'
+    },
+    'ui.router' : ['angular']
+  },
+
+  deps: [
+    // include ng-app packages as dependencies here
+  ]
+
+});
+console.log('Loaded main.js');
